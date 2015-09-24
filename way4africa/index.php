f8f710a0030c1be81e0dd64d6931e5d7d10f0f26
Torian <torian@clickmoremeida.com>
2015-09-24 14:02:57 -0400

    way4africa site

---
+++ way4africa/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<title>Way 4 Africa</title>
+	
+	<link rel="stylesheet/less" type="text/css" href="common/css/main.less" />
+	<script type="text/javascript" src="common/js/less.min.js"></script>
+
+</head>
+<body>
+	<div class="wrapper">
+
+     <?php include('common/includes/header.php'); ?>
+
+		<section class="splash">
+		</section>
+		<div class="content-container">
+			<section class="main-content">
+				<header>
+					WELCOME TO WAY 4 AFRICA
+				</header>
+				<p>
+				    <span style="font-size: 18px;"><u><strong>Karibuni!</strong></u></span>
+				</p>
+				<p>
+				    Welcome to the new WAY website and a new WAY to engage in healthy, respectful, mutually rewarding cross-cultural relationships. While you are here we hope you discover new ways to engage with people and groups from cultures and countries that differ from your own.					
+				    <br><br>
+					WAY is a small educational non-profit organization. Since 2004, WAY has sought to strengthen bridges of understanding between Americans and East Africans as well as between diverse groups of people in the USA. 
+					<br><br>
+					That idea became a reality in 2004 when WAY officially became a 501 (c) (3) non-profit organization. Since that time Sylvia has served as advisor and/or tour organizer for individuals, families, church groups and non-profit organizations traveling to the continent.
+					<br><br>
+					We welcome the opportunity to serve each and every one of you.
+				</p>
+
+				<div style="background-size: 240px 28px;" class="sub-header">
+					AVAILABLE SERVICES
+				</div>
+				<p class="small">
+					-for individuals, churches, schools, colleges and other organizations
+					<br>
+					<ul>
+						<li>Consulting, Speaking, Teaching, Preaching</li>
+						<li>Assisting with preparation for overseas travel</li>
+						<li>Facilitating cross cultural activities and events</li>
+						<li>Providing orientations for international students, visitors and guest, including tours to acquaint them with urban and suburban America</li>
+						<li>Organizing and leading learning/study journeys to East Africa for Americans of all ages</li>
+					</ul>
+
+
+				</p>
+
+			</section>
+			<section class="side-content">
+				<article>
+					<header>
+						UPCOMING TRIPS &amp; EVENTS
+					</header>
+					<hr>
+					<section class="entry">
+						<span class="date"> Feb 2017 </span> <br>
+						<span class="name"> Next Learning/Service Safari </span>
+						<p class="details"> 
+							Contact us if you’re interested in solo or group travel before that time.		
+						</p>
+					</section>
+					<section class="entry">
+						<span class="date"> January 15-18, 2011 </span> <br>
+						<span class="name"> Trip Name Here</span>
+						<p class="details"> 
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+							Duis aliquet lacinia odio ac ultricies.		
+						</p>
+					</section>
+				</article>
+
+<!--			<article>
+					<header>
+						NEWSLETTER SIGN-UP
+					</header>
+					<hr>
+				<form>
+					<p style="color: #47300b;" class="details">
+						Sign up for our email newsletter and stay up-to-date with what we're doing.
+					</p>
+					<input type="text" placeholder="Enter Your Name">
+					<input type="text" placeholder="Your E-Mail">
+					<button class="submit"> Sign-Up</button>
+				</form>
+				</article>
+-->
+			</section>
+		</div> <!-- end of content container -->
+		<div class="bottom"></div>
+     <?php include('common/includes/footer.php'); ?>
+
+	</div>
+</body>
+
+</html> 
---
+++ way4africa/services.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<title>Way 4 Africa</title>
+	
+	<link rel="stylesheet/less" type="text/css" href="common/css/main.less" />
+	<script type="text/javascript" src="common/js/less.min.js"></script>
+
+</head>
+<body>
+	<div class="wrapper">
+     <?php include('common/includes/header.php'); ?>
+		<section class="splash">
+		</section>
+		<div class="content-container">
+			<section class="main-content">
+				<header>
+					SERVICES
+				</header>
+				<p>
+					A WAY Learning/Service Safari is not your typical safari or mission trip. WAY focuses on educating Americans, empowering Africans and establishing cross- cultural relationships that will last a lifetime. WAY Learning/Service Safaris are also more affordable than luxury tours and volunteer opportunities are arranged according to the interest and needs of group members.
+					<br><br>
+					On a WAY Safari you will live simply but comfortably in safe, secure guesthouses, conference centers, hotels or homes rather than five star hotels. You will also have more flexibility, more opportunities for group interaction, more time for in-depth discussions and more personal attention as part of a small group of learners (5-20 people) rather than a large herd of tourist.
+					<br><br>
+					On a WAY Safari you will listen to and learn from the true experts on Africa, namely Africans, as well as, African Americans and others who have spent extended periods of time on the continent.  As Africans discus their joys and challenges you will gleen about their culture, history, economics and politics.
+					<br><br>
+					Our staff of experienced American and Kenyan professionals represents a wide body of expertise guaranteeing hassle-free experience that includes step-by-step assistance with pre-trip planning pertaining to immunizations, visas, packing, safety precautions, familiarity with cultural norms, social etiquette, etc.
+				</p>
+
+			</section>
+<!--
+			<section class="side-content">
+				<article>
+					<header>
+						UPCOMING TRIPS & EVENTS
+					</header>
+					<hr>
+					<section class="entry">
+						<span class="date"> January 15-18, 2011 </span> <br>
+						<span class="name"> Trip Name Here</span>
+						<p class="details"> 
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+							Duis aliquet lacinia odio ac ultricies.		
+						</p>
+					</section>
+					<section class="entry">
+						<span class="date"> January 15-18, 2011 </span> <br>
+						<span class="name"> Trip Name Here</span>
+						<p class="details"> 
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+							Duis aliquet lacinia odio ac ultricies.		
+						</p>
+					</section>
+				</article>
+
+				<article>
+					<header>
+						NEWSLETTER SIGN-UP
+					</header>
+					<hr>
+				<form>
+					<p style="color: #47300b;" class="details">
+						Sign up for our email newsletter and stay up-to-date with what we're doing.
+					</p>
+					<input type="text" placeholder="Enter Your Name">
+					<input type="text" placeholder="Your E-Mail">
+					<button class="submit"> Sign-Up</button>
+				</form>
+				</article>
+
+			</section>
+-->
+		</div> <!-- end of content container -->
+		<div class="bottom"></div>
+	     <?php include('common/includes/footer.php'); ?>
+
+	</div>
+</body>
+
+</html> 
